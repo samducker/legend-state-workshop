df404396cfee71f5475329887a3b72960a47220d
Jay Meistrich <jmeistrich@gmail.com>
2024-11-12 10:52:45 +0000

    Add uuid

--- expo-app/app/(tabs)/index.tsx
+++ expo-app/app/(tabs)/index.tsx
@@ -1,6 +1,25 @@
+import { generateId } from "@/core/generateId";
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
+import { useMemo, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const { getItem, setItem } = useAsyncStorage("my_id");
+  const [id, setId] = useState<string | null>(null);
+
+  // get id from async storage or create it
+  useMemo(() => {
+    getItem().then((id) => {
+      if (id) {
+        setId(id);
+      } else {
+        const newId = generateId();
+        setItem(newId);
+        setId(newId);
+      }
+    });
+  }, []);
+
   return (
     <ScrollView>
       <View style={styles.container}>
