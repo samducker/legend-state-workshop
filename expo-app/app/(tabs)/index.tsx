2c8e34ff7d33b146031b3dffadd83c0b774290d1
Jay Meistrich <jmeistrich@gmail.com>
2024-11-13 12:44:19 +0000

    Persist todos and user to AsyncStorage

--- expo-app/app/(tabs)/index.tsx
+++ expo-app/app/(tabs)/index.tsx
@@ -1,31 +1,11 @@
 import { Todos } from '@/components/Todos';
-import { generateId } from '@/core/generateId';
-import { use$, useObservable } from '@legendapp/state/react';
-import { useAsyncStorage } from '@react-native-async-storage/async-storage';
-import { useMemo } from 'react';
+import { user$ } from '@/core/state';
+import { use$ } from '@legendapp/state/react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
-    const { getItem, setItem } = useAsyncStorage('my_id');
-
-    // Create observable for id
-    const id$ = useObservable<string | null>(null);
-    // Get raw value of id$
-    const id = use$(id$);
-
-    // get id from async storage or create it
-    useMemo(() => {
-        getItem().then((id) => {
-            if (id) {
-                id$.set(id);
-            } else {
-                const newId = generateId();
-                setItem(newId);
-                id$.set(newId);
-            }
-        });
-    }, []);
+    const id = use$(user$.id);
 
     console.log('6 - Home screen');
 
--- expo-app/core/state.ts
+++ expo-app/core/state.ts
@@ -1,7 +1,43 @@
+import { generateId } from '@/core/generateId';
 import { Todo } from '@/core/keelClient';
-import { observable } from '@legendapp/state';
+import { observable, syncState, when } from '@legendapp/state';
+import { observablePersistAsyncStorage } from '@legendapp/state/persist-plugins/async-storage';
+import { synced } from '@legendapp/state/sync';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const todos$ = observable<Record<string, Todo>>({});
+// Setup AsyncStorage plugin with the specific implementation
+const pluginAsyncStorage = observablePersistAsyncStorage({
+    AsyncStorage,
+});
+
+// Setup user$
+export const user$ = observable<{ id: string }>(
+    synced({
+        initial: {},
+        persist: {
+            plugin: pluginAsyncStorage,
+            name: 'user',
+        },
+    }),
+);
+
+// If persist loads without an id, generate one.
+when(syncState(user$).isPersistLoaded, () => {
+    if (!user$.id.peek()) {
+        user$.id.set(generateId());
+    }
+});
+
+// Setup todos$
+export const todos$ = observable<Record<string, Todo>>(
+    synced({
+        initial: {},
+        persist: {
+            plugin: pluginAsyncStorage,
+            name: 'todos',
+        },
+    }),
+);
 
 // A sorted list of todos
 export const todosSorted$ = observable<Todo[]>(() =>
